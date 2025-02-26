15a0b7eadff4137e3b48a927d2cb6d8dc438f61f
lucaserib <lucasemanuelribeiro@hotmail.com>
2025-02-26 07:21:29 -0300

    login screen

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -10,7 +10,10 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Link href="/sign-in">Sign In</Link>
+      <Text className="font-bold my-10 font-rubik text-3xl">
+        Welcome to ReState
+      </Text>
+      <Link href="/signIn">Sign In</Link>
       <Link href="/explore">Explore</Link>
       <Link href="/profile">Profile</Link>
       <Link href="/properties/1">Properties</Link>
